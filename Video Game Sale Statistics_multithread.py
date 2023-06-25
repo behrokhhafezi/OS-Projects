dc52902c1c6da379c08bbf11755b45b51f34a0cc
behrokhhafezi <137639443+behrokhhafezi@users.noreply.github.com>
2023-06-25 07:56:05 +0330

    Update Video Game Sale Statistics_multithread.py

--- Video Game Sale Statistics_multithread.py
+++ Video Game Sale Statistics_multithread.py
@@ -1,17 +1,38 @@
+##########################################################
+# LIBRARIES
+##########################################################
+
 import threading
 import pandas
 
+##########################################################
+# GLOBAL VARIABLES
+##########################################################
+
 Data_Table = pandas.read_csv('video-game-sales.csv')
+
+##########################################################
+# FIND MAX FUNCTION
+##########################################################
 
 def find_max():
     Max_Sale = Data_Table['Global_Sales'].max()
     print("max sale: " , Max_Sale)
 
+##########################################################
+# FIND MIN FUNCTION
+##########################################################
+
 def find_min():
     Min_Sale = Data_Table['Global_Sales'].min()
     print("min sale: " , Min_Sale)
 
+##########################################################
+# MAIN FUNCTION
+##########################################################
+
 def main():
+    # make two thread to execute max and min functions
     max_thread = threading.Thread(target= find_max)
     min_thread = threading.Thread(target= find_min)
 
@@ -20,6 +41,10 @@ def main():
 
     max_thread.join()
     min_thread.join()
+    
+##########################################################
+# USE MAIN FUNCTION
+##########################################################
 
 if __name__ == "__main__":
     main()
