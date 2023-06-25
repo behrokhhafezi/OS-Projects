050e142e6b7733d3410ffffe95240c48a8d44005
behrokhhafezi <137639443+behrokhhafezi@users.noreply.github.com>
2023-06-25 07:53:13 +0330

    Video Game Sale Statistics.py

--- Video Game Sale Statistics.py
+++ Video Game Sale Statistics.py
@@ -1,9 +1,20 @@
+##########################################################
+# LIBRARIES
+##########################################################
+
 import pandas
 
+##########################################################
+# MAIN
+##########################################################
+
+# reading 'video-game-sales.csv' file and make a table out of it
 Data_Table = pandas.read_csv('video-game-sales.csv')
 
+# find maximum global sale in video games
 Max_Sale = Data_Table['Global_Sales'].max()
 
+# find maximum global sale in video games
 Min_Sale = Data_Table['Global_Sales'].min()
 
 print("max sale: " , Max_Sale)
